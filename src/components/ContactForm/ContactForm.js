5b3fedb830ffece0ad5e30633f504bdffed744f9
dymitrbaranowski <dmitribaranowski51@gmail.com>
2023-09-15 10:24:54 +0300

    fox4

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -1,10 +1,67 @@
 import { useState } from 'react';
+import { nanoid } from 'nanoid';
 import propTypes from 'prop-types';
 import { Form, FormBtn, FormItem } from './ContactForm.styled';
 
-const ContactForm = 
+export const ContactForm = ({ handleSubmit }) => {
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
 
+  const nameInputId = nanoid();
+  const numberInputId = nanoid();
 
+  const handleFormSubmit = e => {
+    e.preventDefault();
+    const form = e.currentTarget;
+    handleSubmit({ name: name, number: number });
+    form.reset();
+  };
+
+  const handleChange = e => {
+    const { name, value } = e.target;
+
+    switch (name) {
+      case 'name':
+        setName(value);
+        break;
+      case 'number':
+        setNumber(value);
+        break;
+      default:
+        return;
+    }
+  };
+
+  return (
+    <Form onSubmit={handleFormSubmit}>
+      <label htmlFor={nameInputId}>Name </label>
+      <FormItem
+        type="text"
+        name="name"
+        pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+        title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+        required
+        placeholder="Enter name"
+        value={name}
+        onChange={handleChange}
+      />
+      <label htmlFor={numberInputId}>Number </label>
+      <FormItem
+        type="tel"
+        name="number"
+        pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+        title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+        required
+        placeholder="Enter phone number"
+        value={number}
+        onChange={handleChange}
+      />
+      <FormBtn type="submit">Add contact</FormBtn>
+    </Form>
+  );
+};
+
+export default ContactForm;
 
 ContactForm.propTypes = {
   handleSubmit: propTypes.func.isRequired,
